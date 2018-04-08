028cc91adc913ffd81d38e09c1250daf22612d1a
Malthe <malthedj@hotmail.com>
2018-04-08 20:16:39 +0200

    added fileWriterUnitTests

--- ATMSystem/ATMSystem.Unit.Tests/MiscTests/FileWriterUnitTest.cs
+++ ATMSystem/ATMSystem.Unit.Tests/MiscTests/FileWriterUnitTest.cs
@@ -26,5 +26,31 @@ namespace ATMSystem.Unit.Tests.MiscTests
             //assert
             Assert.IsTrue(File.Exists(path));
         }
+
+        [Test]
+        public void Write_FileDoesNotExist()
+        {
+            //arrange
+            var uut = new FileWriter();
+
+            //act
+            uut.Write("This is only a test2");
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\" + "NotAlog.txt";
+
+            //assert
+            Assert.IsFalse(File.Exists(path));
+        }
+
+        [Test]
+        public void Write_ThrowsException_IfStringIsEmpty()
+        {
+            //arrange
+            var uut = new FileWriter();
+
+            string testString = "";
+            //act
+            //assert
+            Assert.Throws<Exception>(() => { uut.Write(testString); });
+        }
     }
 }
