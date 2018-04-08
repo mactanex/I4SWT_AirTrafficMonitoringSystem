3105b4eaa07f66155d0d0452685d29d0e5bf3e54
Malthe <malthedj@hotmail.com>
2018-04-08 15:54:58 +0200

    added seperationlogger implementation not finished

--- ATMSystem/ATMSystem.Unit.Tests/MiscTests/SeperationLoggerUnitTest.cs
+++ ATMSystem/ATMSystem.Unit.Tests/MiscTests/SeperationLoggerUnitTest.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ATMSystem.Unit.Tests.MiscTests
 {
-    class SeperationLoggerUnitTest
+    [TestFixture]
+    public class SeperationLoggerUnitTest
     {
+        [Test]
+        public void LogSeperation_ExceptionNotThrown()
+        {
+
+        }
+
+        [Test]
+        public void LogSeperation_ExceptionThrown()
+        {
+
+        }
     }
 }
--- ATMSystem/ATMSystem/Misc/SeperationLogger.cs
+++ ATMSystem/ATMSystem/Misc/SeperationLogger.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using ATMSystem.Interfaces;
 
 namespace ATMSystem.Misc
 {
-    class SeperationLogger : ISeperationLogger
+    //  inspired by https://stackoverflow.com/questions/20185015/how-to-write-log-file-in-c
+    public class SeperationLogger : ISeperationLogger
     {
+        private string _path = string.Empty;
+        
+        /// <summary>
+        /// Logs seperation to file
+        /// </summary>
+        /// <param name="seperation"></param>
         public void LogSeperation(ISeperation seperation)
         {
-            throw new NotImplementedException();
+            _path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            try
+            {
+                using (StreamWriter w = File.AppendText(_path+"\\"+"log.txt"))
+                {
+                    w.Write("\r\nLog Entry: ");
+                    w.WriteLine(seperation.TimeOfOccurence);
+                    w.WriteLine("Tags involved: ");
+                    w.WriteLine(seperation.TagsOfInvolvedTracks);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
