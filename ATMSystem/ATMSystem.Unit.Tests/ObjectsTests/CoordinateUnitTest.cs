827116a09ab441f474174d9fe83f4dd4df6c2dfb
Malthe <malthedj@hotmail.com>
2018-04-08 14:26:27 +0200

    added unit test for suggestion class Coordinate

---
+++ ATMSystem/ATMSystem.Unit.Tests/ObjectsTests/CoordinateUnitTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMSystem.Unit.Tests.ObjectsTests
+{
+    class CoordinateUnitTest
+    {
+    }
+}
