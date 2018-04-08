19f576981c1d255f2c83511cfd86bf3ae61945d1
Gobbenobber <gobbenobber@gmail.com>
2018-04-08 18:10:35 +0200

    modified track

--- ATMSystem/ATMSystem/Objects/Track.cs
+++ ATMSystem/ATMSystem/Objects/Track.cs
@@ -4,21 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ATMSystem.Interfaces;
+using ATMSystem.Misc;
 
 namespace ATMSystem.Objects
 {
     class Track : ITrack
     {
         public string Tag { get; set; }
-        public ICoordinate CurrentPosition { get; set; }
+        public ICoordinate CurrentPosition { get ; set; }
         public ICoordinate LastKnownPosition { get; set; }
-        public int CurrentAltitude { get; set; }
-        public int CurrentHorizontalVelocity { get; set; }
-        public int CurrentCompassCourse { get; set; }
 
-        public Track(string tag)
+        public int CurrentAltitude
+        {
+            get => CurrentPosition.y;
+            set => CurrentPosition.y = value;
+        }
+
+        public int CurrentHorizontalVelocity { get; set; }
+        public int CurrentCompassCourse
+        {
+            get => DirectionCalc.CalculateDirection(LastKnownPosition, CurrentPosition);
+            set { throw new NotImplementedException(); }
+        }
+
+        public IDirectionCalc DirectionCalc { get; set; }
+
+
+
+        public void UpdatePosition(ICoordinate coordinate)
+        {
+            LastKnownPosition = CurrentPosition;
+            CurrentPosition = coordinate;
+        }
+
+        public Track(string tag, ICoordinate currentPos)
         {
             Tag = tag;
+            CurrentAltitude = currentPos.y;
+            CurrentCompassCourse = 0;
+            CurrentHorizontalVelocity = 0;
+            CurrentPosition = currentPos;
+            LastKnownPosition = new Coordinate();
+            DirectionCalc = new DirectionCalc();
         }
     }
 }
