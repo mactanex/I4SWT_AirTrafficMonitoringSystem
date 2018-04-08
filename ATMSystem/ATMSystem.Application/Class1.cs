98a9a44e37863bb91bcd7a4ebf8c4dfee1bf4cd4
jonasna <jonasna93@gmail.com>
2018-04-08 18:10:20 +0200

    Added trackcontroller

--- ATMSystem/ATMSystem.Application/Class1.cs
+++ ATMSystem/ATMSystem.Application/Class1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using TransponderReceiver;
 
 namespace ATMSystem.Application
 {
@@ -11,6 +13,25 @@ namespace ATMSystem.Application
     /// </summary>
     public class Class1
     {
-        
+        static void lol(object obj, RawTransponderDataEventArgs args)
+        {
+            foreach (var derp in args.TransponderData)
+            {
+                Console.WriteLine(derp);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        public static void Main(string[] args)
+        {
+            ITransponderReceiver receiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+            receiver.TransponderDataReady += lol;
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+
+        }
     }
 }
