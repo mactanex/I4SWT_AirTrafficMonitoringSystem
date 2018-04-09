70a32e80915a19581e84f406d5865deeb2b1889a
Gobbenobber <gobbenobber@gmail.com>
2018-04-09 17:44:45 +0200

    Added Test Application

---
+++ ATMSystem/ATMSystem.Application/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using ATMSystem.Handlers;
+using ATMSystem.Misc;
+using ATMSystem.Objects;
+using TransponderReceiver;
+
+namespace ATMSystem.Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var myTransponderReceiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+            var myDataConverter = new TransponderDataConverter();
+            var myOutput = new Output();
+
+            var trackController = new TrackController(myTransponderReceiver, myDataConverter, myOutput);
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
