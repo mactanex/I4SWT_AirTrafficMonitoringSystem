778758af050ae878b074b93b1f2665b0eae81b05
Gobbenobber <gobbenobber@gmail.com>
2018-04-25 01:33:26 +0200

    Edited IT 1

--- ATMSystem/ATMSystem.Integration.Tests/IntegrationTest1.cs
+++ ATMSystem/ATMSystem.Integration.Tests/IntegrationTest1.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using ATMSystem.Handlers;
 using ATMSystem.Interfaces;
 using ATMSystem.Misc;
 using NSubstitute;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using TransponderReceiver;
 
 namespace ATMSystem.Integration.Tests
@@ -19,10 +17,7 @@ namespace ATMSystem.Integration.Tests
         private ITrackController _trackController;
         private IMapDrawer _mapDrawer;
         private ITransponderDataConverter _transponderDataConverter;
-        private ISeparationMonitor _separationMonitor;
         private ITransponderReceiver _transponderReceiver;
-
-        private int nEventsRaised = 0;
         
         [SetUp]
         public void SetUp()
@@ -34,16 +29,19 @@ namespace ATMSystem.Integration.Tests
             //included
             _transponderDataConverter = new TransponderDataConverter();
             _trackController = new TrackController(_transponderReceiver, _transponderDataConverter, _mapDrawer);
-            _separationMonitor = new SeparationMonitor(_trackController);
-
-            _separationMonitor.OnSeparationEvent += delegate { nEventsRaised++; };
         }
 
-
         [Test]
-        public void TransponderDataReady_AddedOneTrack_OnSeparationEventRaised0Times()
+        public void TransponderDataReadyEventRaised_TransponderDataConverter_ReturnsCorrectTrack()
         {
             //arrange
+            var fakeTrack = Substitute.For<ITrack>();
+            fakeTrack.Tag = "ATR423";
+            fakeTrack.CurrentPosition.x = 39045;
+            fakeTrack.CurrentPosition.y = 12932;
+            fakeTrack.CurrentAltitude = 14000;
+            fakeTrack.LastSeen = new DateTime(2015,10,6,21,34,56,789);
+           
             var inputStrings = new List<string>
             {
                 "ATR423;" +
@@ -51,128 +49,18 @@ namespace ATMSystem.Integration.Tests
                 "12932;" +
                 "14000;" +
                 "20151006213456789"
-            }; 
+            };
             var args = new RawTransponderDataEventArgs(inputStrings);
-            
-            //act
 
+            //act
             _transponderReceiver.TransponderDataReady += Raise.EventWith(_transponderReceiver, args);
-            
 
-            //assert
-            Assert.That(nEventsRaised, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void TransponderDataReady_AddedTwoTracksNoConflict_OnSeparationEventRaised0Times()
-        {
-            //arrange
-            var track1 = new List<string>
-            {
-                "ATR423;" +
-                "39045;" +
-                "12932;" +
-                "14000;" +
-                "20151006213456789"
-            };
-            var args1 = new RawTransponderDataEventArgs(track1);
-
-            var track2 = new List<string>
-            {
-                "KOL543;" +
-                "80000;" +
-                "45000;" +
-                "9000;" +
-                "20151006213456789"
-            };
-            var args2 = new RawTransponderDataEventArgs(track2);
-
-
-            //act
-
-            _transponderReceiver.TransponderDataReady += Raise.EventWith(_transponderReceiver, args1);
-            _transponderReceiver.TransponderDataReady += Raise.EventWith(_transponderReceiver, args2);
-
-            //assert
-            Assert.That(nEventsRaised, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void TransponderDataReady_AddedTwoTracksInConflict_OnSeparationEventRaised1Time()
-        {
-            //arrange
-            var track1 = new List<string>
-            {
-                "ATR423;" +
-                "39045;" +
-                "12932;" +
-                "14000;" +
-                "20151006213456789"
-            };
-            var args1 = new RawTransponderDataEventArgs(track1);
-
-            var track2 = new List<string>
-            {
-                "RTA322;" +
-                "40000;" +
-                "13000;" +
-                "14100;" +
-                "20151006213456789"
-            };
-            var args2 = new RawTransponderDataEventArgs(track2);
-
-
-            //act
-
-            _transponderReceiver.TransponderDataReady += Raise.EventWith(_transponderReceiver, args1);
-            _transponderReceiver.TransponderDataReady += Raise.EventWith(_transponderReceiver, args2);
-
-            //assert
-            Assert.That(nEventsRaised, Is.EqualTo(1));
-        }
-
-        [Test]
-        public void TransponderDataReady_AddedTwoTracksInConflict_ConflictResolved_OnSeparationEventRaised2Times()
-        {
-            //arrange
-            var track1 = new List<string>
-            {
-                "ATR423;" +
-                "39045;" +
-                "12932;" +
-                "14000;" +
-                "20151006213456789"
-            };
-            var args1 = new RawTransponderDataEventArgs(track1);
-
-            var track2 = new List<string>
-            {
-                "RTA322;" +
-                "40000;" +
-                "13000;" +
-                "14100;" +
-                "20151006213456789"
-            };
-            var args2 = new RawTransponderDataEventArgs(track2);
-
-
-            //act
-
-            _transponderReceiver.TransponderDataReady += Raise.EventWith(_transponderReceiver, args1);
-            _transponderReceiver.TransponderDataReady += Raise.EventWith(_transponderReceiver, args2);
-
-            track2 = new List<string>
-            {
-                "RTA322;" +
-                "40000;" +
-                "13000;" +
-                "9000;" +
-                "20151006213456789"
-            };
-            args2 = new RawTransponderDataEventArgs(track2);
-            _transponderReceiver.TransponderDataReady += Raise.EventWith(_transponderReceiver, args2);
-            //assert
-            Assert.That(nEventsRaised, Is.EqualTo(2));
+            //Assert
+            Assert.That(_trackController.Tracks.First().Key, Is.EqualTo(fakeTrack.Tag));
+            Assert.That(_trackController.Tracks.First().Value.CurrentPosition.x, Is.EqualTo(fakeTrack.CurrentPosition.x));
+            Assert.That(_trackController.Tracks.First().Value.CurrentPosition.y, Is.EqualTo(fakeTrack.CurrentPosition.y));
+            Assert.That(_trackController.Tracks.First().Value.CurrentAltitude, Is.EqualTo(fakeTrack.CurrentAltitude));
+            Assert.That(_trackController.Tracks.First().Value.LastSeen, Is.EqualTo(fakeTrack.LastSeen));
         }
 
     }
