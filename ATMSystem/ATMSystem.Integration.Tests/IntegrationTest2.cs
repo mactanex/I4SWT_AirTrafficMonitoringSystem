31126885322e27dfe302c155010ff3d781a9bd73
Gobbenobber <gobbenobber@gmail.com>
2018-04-25 02:04:30 +0200

    Added IT 2 + IT3

--- ATMSystem/ATMSystem.Integration.Tests/IntegrationTest2.cs
+++ ATMSystem/ATMSystem.Integration.Tests/IntegrationTest2.cs
@@ -18,23 +18,65 @@ namespace ATMSystem.Integration.Tests
         private ITrackController _trackController;
         private IMapDrawer _mapDrawer;
         private ITransponderDataConverter _transponderDataConverter;
-        private ISeparationMonitor _separationMonitor;
         private ITransponderReceiver _transponderReceiver;
-
+        private int nEventsRaised;
         [SetUp]
         public void SetUp()
         {
             //fakes
             _mapDrawer = Substitute.For<IMapDrawer>();
-            
-
-            //driver
-            _transponderReceiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+            _transponderReceiver = Substitute.For<ITransponderReceiver>();
 
             //included
             _transponderDataConverter = new TransponderDataConverter();
             _trackController = new TrackController(_transponderReceiver, _transponderDataConverter, _mapDrawer);
-            _separationMonitor = new SeparationMonitor(_trackController);
+            nEventsRaised = 0;
+
+            _trackController.OnTrackUpdated += delegate { nEventsRaised++; };
+        }
+
+
+        [Test]
+        public void TransponderDataReady_AddedTrack_TrackNotFound_TrackInBounds_OnTrackUpdatedEventRaisedOnce()
+        {
+            //arrange
+            var inputStrings = new List<string>
+            {
+                "ATR423;" +
+                "39045;" +
+                "12932;" +
+                "14000;" +
+                "20151006213456789"
+            };
+            var args = new RawTransponderDataEventArgs(inputStrings);
+
+            //act
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(_transponderReceiver, args);
+
+            //assert
+            Assert.That(nEventsRaised, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TransponderDataReady_AddedTrack_TrackNotFound_TrackOutOfBounds_OnTrackUpdatedEventNotRaised()
+        {
+            //arrange
+            var inputStrings = new List<string>
+            {
+                "ATR423;" +
+                "39045;" +
+                "12932;" +
+                "200;" +
+                "20151006213456789"
+            };
+            var args = new RawTransponderDataEventArgs(inputStrings);
+
+            //act
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(_transponderReceiver, args);
+
+            //assert
+            Assert.That(nEventsRaised, Is.EqualTo(0));
         }
     }
 }
+
